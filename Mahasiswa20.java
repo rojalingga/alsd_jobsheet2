eae50f6eb0d0648044c79d5fd89c05cec76d3679
rojalingga <rojalingga2006@gmail.com>
2025-02-20 16:40:19 +0700

    update method main

--- Mahasiswa20.java
+++ Mahasiswa20.java
@@ -34,4 +34,20 @@ public class Mahasiswa20 {
             return "Kinerja kurang";
         }
     }
+
+    public static void main(String[] args) {
+        Mahasiswa20 mhs1 = new Mahasiswa20();
+
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+
+        mhs1.tampilkanInformasi();
+    }
 }
